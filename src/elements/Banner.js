d9247759931d42b86d7a1d5d35d6f155cef06cf3
EllaPotts <ellapotts@gmail.com>
2023-08-16 17:52:09 -0500

    Testing initial site

---
+++ src/elements/Banner.js
@@ -0,0 +1,11 @@
+import headerImg from "../assets/images/header-img.svg"
+
+return (
+    <div className="banner">
+        <img src={banner} alt="Banner" />
+        <div className="banner-content">
+            <h1>Ella Potts</h1>
+            <p>My banner </p>
+        </div>
+    </div>
+)
